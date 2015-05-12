0c0a43e070014816c2dbf664087fa16eb6557ae8
vinnis211 <vinnis@stlwof.com>
2015-05-11 22:24:26 -0500

    version 2

--- mario.java
+++ mario.java
@@ -1,86 +1,51 @@
 /**
- * Write a description of class mario here.
- * 
- * @author Vinnis L. Bryant
- * @version 2
+ * Created by omx on 5/10/2015.
  */
+import pyramids.pyramidToFile;
+import pyramids.pyramidToScreen;
 
 import java.util.Scanner;
-public class mario
-{
-   public static void main(String[] args) {
-       
-       int pHeight = getHeight();
-       makePyramid(pHeight);
-       
-    }
-   
-   /*****************************************
-   * Request and store a height for Mario's *
-   * pyramid                                *
-   *****************************************/
-   public static int getHeight () {
-      
-      int inputHeight = 0;
-      Scanner in = new Scanner(System.in);
-    
-      do
-      {
-        System.out.print("Height: ");
-        inputHeight = in.nextInt();
-      }  while (inputHeight < 0 || inputHeight > 23);
-    
-      return inputHeight;
 
-    }
-   
-   /*****************************************
-   * Build pyramid using two loops to pad  *
-   * each row with spaces then #s. The top *
-   * starts with # then susbsequent rows   *
-   * stair step by additional # per row.   *
-   *****************************************/
- 
-   public static void makePyramid(int height) {
-   
-       int maxcol = height + 1;
-       int i;
-       int s = maxcol;
-       int spaceLooper;
-       int sharpLooper;
-       String row = "";
-       System.out.print(row);
-       
-       for(i = 2; i <= maxcol; i++) 
-        
-        {
-          
-            spaceLooper = s - 2;
-            
-                while (spaceLooper > 0)
-                     { 
-                        System.out.print(" ");
-                        spaceLooper--;
-                     }
-                 s--;    
-            
-              
-              sharpLooper = i;   
-              while (sharpLooper > 0)
-              {
-              System.out.print("#");
-              sharpLooper--;
-              }
-              
-              System.out.print("\n");
-                
+public class mario{
+
+    public static void main(String[] args) {
+
+        String outputType = getOutputType();
+
+        switch (outputType) {
+            case "S":
+            case "s":
+                pyramidToScreen s_pyramid = new pyramidToScreen();
+                s_pyramid.build();
+                break;
+            case "F":
+            case "f":
+                pyramidToFile f_pyramid = new pyramidToFile();
+                f_pyramid.build();
+                break;
+            default:
+                break;
+
         }
+    }
 
-       
+    /*****************************************
+     * Request and store output optioon		 *
+     *                                        *
+     *****************************************/
+    public static String getOutputType () {
 
-       
-   }
-      
+        String outType = "";
+        Scanner in2 = new Scanner(System.in);
+
+        do
+        {
+            System.out.print("Output to (S)sreen or (F)file: ");
+            outType = in2.next();
+        }  while (!outType.equals("S") && !outType.equals("F") && !outType.equals("s") && !outType.equals("f"));
+
+        return outType;
+
+    }
+
 }
-
-
